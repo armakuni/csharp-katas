afe3df618f572337243c0207d3b9f62e3d4cb177
Andrew Gibson <andrew.gibson@armakuni.com>
2023-09-29 14:04:51 +0100

    bank account kata with nunit

---
+++ bank-account/nunit/BankAccountKata.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BankAccountKata.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
